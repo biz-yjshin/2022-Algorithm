54ae2adb943e0cdb54aff45f421c4dbcc545be1f
biz-yjshin <biz.yjshin@gmail.com>
2022-08-16 17:15:13 +0900

    [백준][11단계] 11650 - 좌표 정렬하기

--- baekjoon/11/11650CoordinatesSort.java
+++ baekjoon/11/11650CoordinatesSort.java
@@ -6,36 +6,89 @@
  * 
  */
 
+/*
+
+중첩 for문으로 데이터를 비교하니 시간 초과가 되었다.
+다른 블로그를 참고하니 Arrays.sort() 함수를 사용하여 해당 문제를 풀었다.
+
+Arrays.sort()는 일차원 배열은 가능하지만 이차원 배열은 불가능하기에 람다식을 사용하여 Arrays.sort() 확장할 수 있어야 한다.
+
+1. Scanner + println
+2. Scanner + StringBuilder
+
+참고 ) https://st-lab.tistory.com/110
+
+*/
+import java.util.Scanner;
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 class CoordinatesSort { // CoordinatesSort
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
         int[][] coordinates = new int[N][2];
 
-        for(int i = 0; i < N; i++){
-            coordinates[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        }
+		for(int i = 0; i < N; i++) {
+			coordinates[i][0] = sc.nextInt();
+			coordinates[i][1] = sc.nextInt();
+		}
 
-        // for(int i = 0; i < N; i++){
-        //     int temp = -1;
-        //     for(int j = i + 1; j < N; j++) {
-        //         if(sortArr[i] > sortArr[j]) {
-        //             temp = sortArr[i];
-        //             sortArr[i] = sortArr[j];
-        //             sortArr[j] = temp;
-        //         }
-        //     }
-        //     sb.append(sortArr[i]).append('\n');
-        // }
-        // System.out.println(sb);
+        Arrays.sort(coordinates, (e1, e2) -> {
+            if(e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++){
+            sb.append(coordinates[i][0] + " " + coordinates[i][1]).append("\n");
+        }
+        System.out.println(sb);
     }
 }
+
+/*
+// 처음 푼 방법 - 시간 초과
+import java.util.Scanner;
+import java.io.IOException;
+import java.util.Arrays;
+
+class CoordinatesSort { // CoordinatesSort
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] coordinates = new int[N][2];
+
+		for(int i = 0; i < N; i++) {
+			coordinates[i][0] = sc.nextInt();
+			coordinates[i][1] = sc.nextInt();
+		}
+
+        for(int i = 0; i < N - 1; i++) {
+            for(int j = 0; j < N; j++) {
+                if(coordinates[i][0] > coordinates[j][0]) {
+                    int[] temp = new int[2];
+                    temp = coordinates[i];
+                    coordinates[i] = coordinates[j];
+                    coordinates[j] = temp;
+                }
+                if(coordinates[i][0] == coordinates[j][0]) {
+                    if(coordinates[i][1] > coordinates[j][1]) {
+                        int[] temp = new int[2];
+                        temp = coordinates[i];
+                        coordinates[i] = coordinates[j];
+                        coordinates[j] = temp;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < N; i++){
+            System.out.println(coordinates[i][0] + " " + coordinates[i][1]);
+        }
+    }
+}
+ */
