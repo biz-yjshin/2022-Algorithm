2b444916dd7eda761f9e4a16b8343ed54edf472c
biz-yjshin <biz.yjshin@gmail.com>
2022-10-11 17:12:35 +0900

    [LeetCode] Easy 단계 - 21. Merge Two Sorted Lists

--- leetCode/easy/21MergeTwoSortedLists.js
+++ leetCode/easy/21MergeTwoSortedLists.js
@@ -1,6 +1,6 @@
 /**
 
-  날짜 : 2022.04.19 | 2022.08.19
+  날짜 : 2022.04.19 | 2022.08.19 | 2022.10.11
   난이도 : Easy
   제목 : 21. Merge Two Sorted Lists
   URL : https://leetcode.com/problems/merge-two-sorted-lists/
@@ -19,30 +19,13 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
-    if(list1.val == null && list2.val == null) {
-        return null;
-    } else if(list1.val == null) {
-        return list2;
-    } else if(list2.val == null) {
-        return list1;
-    }
-
-    let curList = new ListNode(undefined, undefined);
-
-    do {
-        let nextList = new ListNode(undefined, undefined);
-        if(list1.val < list2.val) {
-            nextList.val =  nextList.val ? nextList.val : list1.val;
-            list1.val = list1.next.val;
-            list1.next = list1.next.next;
-        } else {
-            nextList.val =  nextList.val ? nextList.val : list2.val;
-            list2.val = list2.next.val;
-            list2.next = list2.next.next;
-        }
-        curList.next = nextList;
-    } while(list1.next == null && list2.next == null);
-    curList.val = curList.next
-    return curList;
-};
+ var mergeTwoLists = function(list1, list2) {
+    if(!list1 || !list2) return(list1 ? list1 : list2);
+      if(list1.val < list2.val) {
+          list1.next = mergeTwoLists(list1.next, list2);
+          return list1;
+      } else {
+          list2.next = mergeTwoLists(list1, list2.next);
+          return list2;
+      }
+  };
